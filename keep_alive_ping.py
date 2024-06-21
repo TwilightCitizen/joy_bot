f7a39a1812a551dc17c2438aa780313c8083e7c9
TwilightCitizen <somewhat.esoteric@icloud.com>
2024-06-21 10:58:01 -0400

    No reason to disable keep alive ping, so removed enabled boolean.  Modified guards to check for scheduled job before starting or stopping (adding or removing) it.  Embedded the Kik client ping in another method to augment with console logging.

--- keep_alive_ping.py
+++ keep_alive_ping.py
@@ -15,29 +15,30 @@ from int_range import IntRange
 class KeepAlivePing:
     def __init__(self, kik_client: KikClient, background_scheduler: BackgroundScheduler):
         self.interval: IntRange = IntRange(bottom=1, top=30, default=10)
-        self.enabled: bool = True
         self.id: str = "KEEP_ALIVE_PING"
         self.kik_client: KikClient = kik_client
         self.background_scheduler: BackgroundScheduler = background_scheduler
 
+    def _send_keep_alive_ping(self):
+        self.kik_client.send_ping()
+        print("Sent Ping")
+
     def start(self) -> None:
-        if not self.enabled:
+        if self.background_scheduler.get_job(self.id):
             return
 
         print("Starting Keep Alive Ping")
 
         self.background_scheduler.add_job(
-            self.kik_client.send_ping,
+            self._send_keep_alive_ping,
             "interval",
             minutes=self.interval.get(),
             id=self.id,
         )
 
     def stop(self) -> None:
-        if not self.enabled:
+        if not self.background_scheduler.get_job(self.id):
             return
 
         print("Stopping Keep Alive Ping")
-
-        if self.background_scheduler.get_job(self.id):
-            self.background_scheduler.remove_job(self.id)
+        self.background_scheduler.remove_job(self.id)
