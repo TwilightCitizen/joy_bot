cdb54c9ce9cd0b84ab8ea328ce0a577e652b9dfc
TwilightCitizen <somewhat.esoteric@icloud.com>
2024-06-21 20:58:36 -0400

    Implemented GreetingPromoted.

---
+++ greetings_and_farewells/greeting_promoted.py
@@ -0,0 +1,32 @@
+# Library Imports
+
+from kik_unofficial.client import KikClient
+
+# Application Imports
+
+from greeting_or_farewell import GreetingOrFarewell
+
+
+# Definitions
+
+class GreetingPromoted(GreetingOrFarewell):
+    def __init__(
+        self,
+        kik_client: KikClient
+    ):
+        super().__init__(
+            kik_client=kik_client,
+            message="Congrats, {demoted}...  {admin} must like you!"
+        )
+
+    def greet_promoted(
+        self,
+        group_jid: str,
+        promoted: str | None = None,
+        admin: str | None = None
+    ) -> None:
+        super().greet_or_farewell(
+            group_jid,
+            promoted=promoted,
+            admin=admin
+        )
