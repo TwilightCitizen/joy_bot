87202f9508ca1cd2a618397a88af1b446aedaf89
TwilightCitizen <somewhat.esoteric@icloud.com>
2024-06-21 20:37:46 -0400

    Implemented FarewellDemoted.

---
+++ greetings_and_farewells/farewell_demoted.py
@@ -0,0 +1,32 @@
+# Library Imports
+
+from kik_unofficial.client import KikClient
+
+# Application Imports
+
+from greeting_or_farewell import GreetingOrFarewell
+
+
+# Definitions
+
+class FarewellDemoted(GreetingOrFarewell):
+    def __init__(
+        self,
+        kik_client: KikClient
+    ):
+        super().__init__(
+            kik_client=kik_client,
+            message="Aw...  Sorry, {demoted}, but it's {owners}'s group!"
+        )
+
+    def farewell_demoted(
+        self,
+        group_jid: str,
+        demoted: str | None = None,
+        owner: str | None = None
+    ) -> None:
+        super().greet_or_farewell(
+            group_jid,
+            demoted=demoted,
+            owner=owner
+        )
